2539163616f35ae8f1723fbc95494953fddc5fdd
Dev <ramesh.murugaswamy@gmail.com>
2014-12-22 15:55:15 -0500

    Initial commit.

---
+++ test/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Dev on 12/22/14.
+//  Copyright (c) 2014 Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *headLable;
+
+@end
+
